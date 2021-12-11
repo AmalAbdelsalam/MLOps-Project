fd7decf2c48af0cc242c0dac07e8ef2835a25e92
Amal Abdelsalam <amal.a.halaby@gmail.com>
2021-12-11 20:37:18 +0200

    update/clean

--- assignment-2/app/AreaCalculator.py
+++ assignment-2/app/AreaCalculator.py
@@ -1,9 +1,6 @@
 '''
 A function to calculate the area of a land (acers), given its length & width (feets)
 '''
-def calculate_area_acres(length=None, width=None):
-    if not length or not width:
-        return 'No input provided'
-    
+def calculate_area_acres(length, width):    
     acres = (length * 1.0 * width ) / 43560
     return acres
